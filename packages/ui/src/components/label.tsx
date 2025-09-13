03729581e68a4fdf74e4f97c016c8a93a9bd8c39
ruru <ruru.dev07@gmail.com>
2025-09-13 16:33:17 +0530

    fix: linter issue with ui package

--- packages/ui/src/components/label.tsx
+++ packages/ui/src/components/label.tsx
@@ -1,24 +1,23 @@
 "use client";
 
+import { cn } from "@noutify/ui/lib/utils";
 import * as React from "react";
 
-import { cn } from "@noutify/ui/lib/utils";
-
 const Label = React.forwardRef<
-  HTMLLabelElement,
-  React.LabelHTMLAttributes<HTMLLabelElement>
+	HTMLLabelElement,
+	React.LabelHTMLAttributes<HTMLLabelElement>
 >(
-  // eslint-disable-next-line react/prop-types
-  ({ className, ...props }, ref) => (
-    <label
-      ref={ref}
-      className={cn(
-        "text-sm font-medium leading-4 text-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-        className
-      )}
-      {...props}
-    />
-  )
+	// eslint-disable-next-line react/prop-types
+	({ className, ...props }, ref) => (
+		<label
+			ref={ref}
+			className={cn(
+				"text-sm font-medium leading-4 text-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+				className,
+			)}
+			{...props}
+		/>
+	),
 );
 Label.displayName = "Label";
 
