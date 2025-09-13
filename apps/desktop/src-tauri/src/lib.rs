7fbdb26be1104a77edb1e7992c3e1ca43e71dc0f
ruru <ruru.dev07@gmail.com>
2025-09-13 15:45:19 +0530

    faet: setup cargo monorepo and linters

---
+++ apps/desktop/src-tauri/src/lib.rs
@@ -0,0 +1,11 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use ipc::commands;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![commands::add, commands::greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ crates/ipc/src/commands.rs
@@ -0,0 +1,14 @@
+#[tauri::command]
+pub fn my_custom_command() {
+  println!("I was invoked from JavaScript!");
+}
+
+#[tauri::command]
+pub fn add(a: i32, b: i32) -> i32 {
+  a + b
+}
+
+#[tauri::command]
+pub fn greet(name: String) -> String {
+  format!("hello {}!", name)
+}
