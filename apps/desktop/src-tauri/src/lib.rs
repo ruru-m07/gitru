78e878254a0449fb4366ea676562581223c891e2
ruru <ruru.dev07@gmail.com>
2025-11-09 21:36:25 +0530

    feat: integrate updater plugin and update configurations

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,11 +1,13 @@
 use git;
 use ipc;
+use tauri_plugin_updater;
 use tauri_plugin_window_state;
 use watcher;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
