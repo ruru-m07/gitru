c125e3ae9a0d3ea18eccd9a3ebbc632b2248d683
ruru <ruru.dev07@gmail.com>
2025-11-15 15:07:03 +0530

    fix(mac): improved dmg image alignment and size

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ use watcher;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
