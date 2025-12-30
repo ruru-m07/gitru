04d12798254aa0c4c3bb03179d9e8e6f030eb3f5
ruru <ruru.dev07@gmail.com>
2025-12-30 22:20:01 +0530

    feat: ability to make commits

--- apps/desktop/src/routes/app/git/route.tsx
+++ apps/desktop/src/routes/app/git/route.tsx
@@ -22,6 +22,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@gitru/ui/components/dialog";
+import { Group, GroupSeparator } from "@gitru/ui/components/group";
 import { Input } from "@gitru/ui/components/input";
 import {
   InputGroup,
@@ -35,6 +36,10 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
+  Menu,
+  MenuItem,
+  MenuPopup,
+  MenuTrigger,
 } from "@gitru/ui/components/menu";
 import {
   ResizableHandle,
@@ -85,6 +90,7 @@ import {
 } from "@/lib/time";
 import {
   getCommitHistory,
+  useCurrentBranch,
   useRepositoryActions,
   useStatus,
 } from "@/state/hooks";
@@ -404,41 +410,7 @@ function GitPageLayout() {
                         </>
                       )}
                     </div>
-                    <div className="shrink-0 border-l flex flex-col gap-2 justify-between items-center border-t px-2 py-2 bg-accent dark:bg-accent/10">
-                      <Input
-                        placeholder="Summary (required)"
-                        className="h-8 _border-border dark:bg-background!"
-                      />
-                      <InputGroup className="dark:bg-background!">
-                        <InputGroupTextarea
-                          placeholder="Description"
-                          className="dark:bg-background!"
-                        />
-                        <InputGroupAddon align="block-end">
-                          <Button
-                            variant="ghost"
-                            size="icon-xs"
-                            className="rounded-full opacity-50 hover:opacity-100"
-                            aria-label="Add Co Authors"
-                          >
-                            <UserPlus size={16} />
-                          </Button>
-                          <Separator
-                            orientation="vertical"
-                            className={"h-[80%] mx-0"}
-                          />
-                          <Button
-                            variant="ghost"
-                            size="icon-xs"
-                            className="rounded-full opacity-50 hover:opacity-100"
-                            aria-label="Add files"
-                          >
-                            <Sparkles size={16} />
-                          </Button>
-                        </InputGroupAddon>
-                      </InputGroup>
-                      <Button className="w-full">Commit to main</Button>
-                    </div>
+                    <WriteCommitBox />
                   </TabsPanel>
                   <TabsPanel value="tab-2" className={"h-full"} tabIndex={-1}>
                     <ScrollArea
@@ -916,5 +888,65 @@ const DiscardChangesDialog = memo(function DiscardChangesDialog({
         </DialogFooter>
       </DialogContent>
     </Dialog>
+  );
+});
+
+const WriteCommitBox = memo(function WriteCommitBox() {
+  const { data: currentBranch } = useCurrentBranch();
+  return (
+    <div>
+      <div className="shrink-0 border-l flex flex-col gap-2 justify-between items-center border-t px-2 py-2 bg-accent dark:bg-accent/10">
+        <Input
+          placeholder="Summary (required)"
+          className="h-8 _border-border dark:bg-background!"
+        />
+        <InputGroup className="dark:bg-background!">
+          <InputGroupTextarea
+            placeholder="Description"
+            className="dark:bg-background!"
+          />
+          <InputGroupAddon align="block-end">
+            <Button
+              variant="ghost"
+              size="icon-xs"
+              className="rounded-full opacity-50 hover:opacity-100"
+              aria-label="Add Co Authors"
+            >
+              <UserPlus size={16} />
+            </Button>
+            <Separator orientation="vertical" className={"h-4 mx-0"} />
+            <Button
+              variant="ghost"
+              size="icon-xs"
+              className="rounded-full opacity-50 hover:opacity-100"
+              aria-label="Add files"
+            >
+              <Sparkles size={16} />
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+        <Group aria-label="Subscription actions" className="w-full">
+          <Button className="flex-1">Commit to {currentBranch?.name}</Button>
+          <GroupSeparator className="bg-primary/72" />
+          <Menu>
+            <MenuTrigger
+              render={
+                <Button
+                  aria-label="Copy options"
+                  size="icon"
+                  className="rounded-r-lg!"
+                />
+              }
+            >
+              <ChevronDownIcon className="size-4" />
+            </MenuTrigger>
+            <MenuPopup align="end" className={"w-full"}>
+              <MenuItem>Empty Commit</MenuItem>
+              <MenuItem>Amend Last Commit</MenuItem>
+            </MenuPopup>
+          </Menu>
+        </Group>
+      </div>
+    </div>
   );
 });
