9748e62d10e3fb4372a4481640d91e3665970de2
ruru <git@ruru.build>
2026-03-14 14:09:15 +0530

    feat: add type annotation for metadata in page component

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,12 +1,12 @@
+import { Metadata } from "next";
 import HomePage from "@/components/homePage";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Gitru",
   description: "A modern Git client for humans.",
+  icons: new URL("/favicon.ico", "https://gitru.app"),
 };
 
 export default function Page() {
-  return (
-    <HomePage />
-  );
+  return <HomePage />;
 }
