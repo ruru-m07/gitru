6bd240de0e0f7be0acc25237c30ea8f1c24e02cc
ruru <git@ruru.build>
2026-05-29 18:30:47 +0530

    feat: added mascot page and setup og images

--- apps/web/app/(home)/mascot/page.tsx
+++ apps/web/app/(home)/mascot/page.tsx
@@ -1,5 +1,15 @@
+import Blueprint from "./blueprint";
+
 const MascotPage = () => {
-  return <div>asƒ</div>;
+  return (
+    <div className="w-full">
+      <div className="max-w-(--container-width) px-(--container-gutter) mx-auto">
+        <div className="flex justify-center w-full">
+          <Blueprint className="w-full h-fit" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MascotPage;
